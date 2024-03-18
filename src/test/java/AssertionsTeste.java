4af8fb1476a226a68384803804c0c2605c1e0ca6
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2024-03-18 13:11:26 -0300

    Testando arrays com assertions

---
+++ src/test/java/AssertionsTeste.java
@@ -0,0 +1,26 @@
+import org.example.Pessoa;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+public class AssertionsTeste {
+    @Test
+    void validarLancamentos(){
+        int[] primeiroLancamento = {10,20,30,40,50};
+        int[] segundoLancamento = {10,5,2,3,10};
+        int[] terceiroLancamento = {10,20,30,40,50};
+        // Comparando Arrays
+        Assertions.assertNotEquals(primeiroLancamento, segundoLancamento);
+        Assertions.assertArrayEquals(primeiroLancamento, terceiroLancamento);
+    }
+
+    @Test
+    void validarSeObjetoNulo(){
+        Pessoa pessoa = null;
+        Assertions.assertNull(pessoa);
+        pessoa = new Pessoa("Luciano", LocalDateTime.now());
+        Assertions.assertNotNull(pessoa);
+    }
+    
+}
