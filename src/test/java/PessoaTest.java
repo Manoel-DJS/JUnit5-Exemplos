beeab298780846852c7079e42c263ad6c4c5bae1
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2024-03-18 11:21:19 -0300

    Validações assertEquals, assertTrue, assertFalse

--- src/test/java/PessoaTest.java
+++ src/test/java/PessoaTest.java
@@ -6,15 +6,17 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class PessoaTest {
+
         @Test
     void deveCalcularIdadeCorretamente() {
             Pessoa veronica = new Pessoa("Verônica", LocalDateTime.of(2002, 5, 14, 15,0,0));
-            //
+            // (assertEquals) Passar o valor esperado // valor a ser testado
             Assertions.assertEquals(21, veronica.getIdade());
         }
 
         @Test
     void deveRetornarMaiorIdade() {
+            // (assertTrue) or (assertFalse) Testando se a condição é verdadeira
             Pessoa veralucia = new Pessoa("Veralucia", LocalDateTime.of(2002, 5, 14, 15,0,0));
             Assertions.assertTrue(veralucia.ehMaiorIdade());
             Pessoa Joao = new Pessoa("João Paulo", LocalDateTime.of(2012,5,30,15,0,0));
